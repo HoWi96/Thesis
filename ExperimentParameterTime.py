9fc15f12e17b1605f15e486166e04bc82255a2f2
HoWi96 <15106404+HoWi96@users.noreply.github.com>
2019-03-04 16:53:28 +0100

    Make 3D Plots of Uncertainty and DP

--- ExperimentParameterTime.py
+++ ExperimentParameterTime.py
@@ -33,7 +33,7 @@ def experimentParameterTime(uncertainty = 0.2):
     totalEnergy = sum(data)/4
 
     #15min,30min,1h,2h,3h, 4h,8h,24h,72h,120h 
-    periods = [.25,.5,1,2,3,4,5,8,12,16,24,72,120,720] #divisors of 720 
+    periods = [.25,.5,1,2,3,4,5,8,12,16,24,48,72,120,720] #divisors of 720 
     varEnergy = np.zeros(len(periods))
     for i in range(len(periods)):
         p = periods[i]
@@ -74,11 +74,54 @@ def experimentParameterUncertainty(period = 4):
     
 # In[] MAIN
 if __name__ == "__main__":
-    plt.close('all')
-    experimentParameterUncertainty(1)
-    experimentParameterUncertainty(4)
-    experimentParameterUncertainty(720)
-    experimentParameterTime(uncertainty = 0.01)
-    experimentParameterTime(uncertainty = 0.2)
-    experimentParameterTime(uncertainty = 0.4)
+    plt.close("all")
+    
+#    #2D PLOTS
+#    plt.close('all')
+#    experimentParameterUncertainty(1)
+#    experimentParameterUncertainty(4)
+#    experimentParameterUncertainty(720)
+#    experimentParameterTime(uncertainty = 0.01)
+#    experimentParameterTime(uncertainty = 0.2)
+#    experimentParameterTime(uncertainty = 0.4)
+    
+    #3D PLOTS
+#    #Total Overview  
+#    uncertainty = np.linspace(0,1,20)
+#    periods = np.array([.25,.5,1,2,3,4,5,8,12,16,24,48,72,120,720])
+        
+    #Region of Interest
+    uncertainty = np.linspace(0,.5,10)
+    periods = np.array([.25,.5,1,2,3,4,5,8,12,24])
+    
+    #Process Data
+    def processEnergyRatio(uncertainty,periods):
+        data = preprocessData(dataPath="DataJune2017.csv")["AGG2017"]
+        hours = int(data.shape[0]/4)
+        totalEnergy = sum(data)/4
+        varEnergy = np.zeros((len(uncertainty),len(periods),))
+        
+        for i in range(len(uncertainty)):
+            for j in range(len(periods)):
+                
+                for n in range(int(hours/periods[j])):
+                    # total energy = certain energy/period*periods*duration/period
+                    varEnergy[i,j] += data[int(n*4*periods[j]):int((n+1)*4*periods[j])].quantile(uncertainty[i])*periods[j]
+        return varEnergy/totalEnergy*100
+       
+    X,Y = np.meshgrid(periods,uncertainty)
+    Z = processEnergyRatio(uncertainty,periods)
+    
+    
+    #3D Surface Plot
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    ax.plot_surface(X, Y*100, Z, rstride=1, cstride=1,cmap='coolwarm',edgecolor='none')
+    ax.set_title('Ratio of Offered Energy to Total Available Energy')
+    ax.set_ylabel('Uncertainty [%]')
+    ax.set_xlabel('Delivery Period [h]')
+    ax.set_zlabel('Energy Ratio [%]')
+    
+    
+    
     
