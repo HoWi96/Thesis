c8675cb15844967f09795b177882637bdfc07516
HoWi96 <15106404+HoWi96@users.noreply.github.com>
2019-05-12 18:24:44 +0200

    Update Methodology_Generality_Illustration.py
    Changed Reserves into Flexibility

--- Methodology_Generality_Illustration.py
+++ Methodology_Generality_Illustration.py
@@ -39,13 +39,12 @@ plt.close("all")
 fig,axes = plt.subplots(3,1)
 
 #Make different plots
-time = np.arange(START/4,STOP/4,0.25)
+time = np.arange(0,72,0.25)
 
 axes[0].plot(time,wind,color = "blue")
 axes[0].fill_between(time,wind,np.where(wind>10, wind-10, 0),color = "blue",alpha = 0.1)
 axes[0].plot(time,np.where(wind>10, wind-10, 0), color = "orange")
 axes[0].fill_between(time,np.where(wind>10, wind-10, 0),0,color = "orange",alpha = 0.1)
-
 
 axes[1].plot(time,solar,color = "blue")
 axes[1].fill_between(time,solar,np.where(solar>10, solar-10, 0),color = "blue",alpha = 0.1)
@@ -53,18 +52,18 @@ axes[1].plot(time,np.where(solar>10, solar-10, 0), color = "orange")
 axes[1].fill_between(time,np.where(solar>10, solar-10, 0),0,color = "orange",alpha = 0.1)
 
 
-axes[2].fill_between(time,100,demand,color="orange",label = "Downward Reserves",alpha = 0.1)
-axes[2].plot(time,demand,label = "Normal Consumption",color = "blue")
-axes[2].fill_between(time,30,demand,color="blue",label = "Upward Reserves",alpha = 0.1)
+axes[2].fill_between(time,100,demand,color="orange",label = "Downward Flexibility",alpha = 0.1)
+axes[2].plot(time,demand,label = "Real-Time Consumption",color = "blue")
+axes[2].fill_between(time,30,demand,color="blue",label = "Upward Flexibility",alpha = 0.1)
 axes[2].fill_between(time,30,0,color="green",label = "Shedding Limit (30MW)",alpha = 0.1)
 
 #Annotations
-titles = ["Wind 100MWp","Solar 100MWp","Demand 100MWp"]
+titles = ["Flexibility Wind 100MWp","Flexibility Solar 100MWp","Flexibility Demand 100MWp"]
 
 for i in (0,1,2):
     axes[i].set_title(titles[i])
     axes[i].set_ylim(0,100)
     axes[i].set_ylabel("Volume [MW]")
     axes[i].set_xlabel("Time [h]")
-    axes[i].legend(("Normal Production","Curtailed Production","Upward Reserves (10MW)","Downward Reserves"))
+    axes[i].legend(("Real-Time Production","Curtailed Real-Time Production","Upward Flexibility (10MW)","Downward Flexibility"))
 axes[2].legend()
