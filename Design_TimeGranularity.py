0a282f0ff953d488da4c4bb6929e28f050f9a6cb
HoWi96 <15106404+HoWi96@users.noreply.github.com>
2019-05-21 13:14:52 +0200

    Results_Granularity Finished

--- Design_TimeGranularity.py
+++ Design_TimeGranularity.py
@@ -11,45 +11,55 @@ import matplotlib.pyplot as plt
 
 solarRaw = pd.read_csv("Data/SolarForecastJune2017.csv")["RealTime"]
 windRaw = pd.read_csv("Data/WindForecastJune2017.csv")["RealTime"]
+demandRaw = pd.read_csv("Data/LoadForecastJune2017.csv")["RealTime"]
 
-#%% PREPROCESS DATA
+#%% Preprocess Data Data
 
-SOLAR_MONITORED = 2952.78
-SOLAR_INSTALLED = 30
-WIND_MONITORED = 2424.07
-WIND_INSTALLED = 100
+WINDINSTALLED = 2403.17
+SOLARINSTALLED = 2952.78
+DEMANDPEAK = 11742.29  
 
-wind = windRaw*WIND_INSTALLED/WIND_MONITORED
-solar = solarRaw*SOLAR_INSTALLED/SOLAR_MONITORED
-agg = wind + solar
+solar = solarRaw*100/SOLARINSTALLED
+wind = windRaw*100/WINDINSTALLED
+demand = demandRaw*100/DEMANDPEAK-30
+agg = solar*0.25 + wind*0.75
+x = np.arange(0,5,0.01)
 
-df = pd.DataFrame(data={"wind":wind,"solar":solar,"agg":agg})
+df = pd.DataFrame(data={"solar":solar,"wind":wind,"agg":agg,"demand":demand})
 
 #%% PROCESS DATA
 plt.close("all")    
 
-TIME_QUANTILE = 10 #round(norm.cdf(2)*100,2)
-#TIME_QUANTILE = 84.13 #round(norm.cdf(1)*100,2)
-TIME_TOTAL = 720.#h
-TIME_GRANULARITY = np.array([.25,.5,1,2,3,4,5,6,8,9,10,12,15,16,18,20,24,30,40,48,50,60,72,80,90,120,144,180])#h
+RELIABILITY = 90 
+TIME_TOTAL = int(df.shape[0]/4)
+#TIME_GRANULARITY = np.array([.25,.5,1,2,3,4,5,6,8,9,10,12,15,16,18,20,24,27,30,32,36,40,42,45,48])#h
+TIME_GRANULARITY = np.arange(0.25,48,0.5)
 TIME_GROUPS = np.array(TIME_TOTAL/TIME_GRANULARITY,dtype="int")
 
-volume = np.zeros((TIME_GRANULARITY.size,3))
+volume = np.zeros((TIME_GRANULARITY.size,4))
 
 for i,gran in enumerate(TIME_GRANULARITY):
     for k in range(0,TIME_GROUPS[i]):
-        volume[i] += np.array(df[int(k*4*gran):int((k+1)*4*gran)].quantile(TIME_QUANTILE/100))
+        volume[i] += np.array(df[int(k*4*gran):int((k+1)*4*gran)].quantile((100-RELIABILITY)/100))
     volume[i] = volume[i]/TIME_GROUPS[i] #taking the mean
     
 volumedf = pd.DataFrame(index = TIME_GRANULARITY,
-             data = {"wind":volume[:,0],
-                     "solar":volume[:,1],
-                     "agg":volume[:,2]})
+                         data = {"solar":   volume[:,0],
+                                 "wind":    volume[:,1],
+                                 "agg":     volume[:,2],
+                                 "demand":  volume[:,3]})
     
-(volumedf["wind"].max() - volumedf["wind"]).plot(label = "Reliability "+str(100-TIME_QUANTILE)+"%" )
+(df.mean() - volumedf).plot(label = "Reliability "+str(RELIABILITY)+"%" )
+
 plt.xlabel('Time Granularity [$\Delta$h]')
 plt.ylabel('Lost Volume [MW]')
-plt.title("Lost Volume By Time Granularity")
-plt.suptitle("Downward Reserves 100MWp Wind")
-plt.legend()
+plt.title("Lost Volume By Time Resolution"+
+             "\nMean Effective Volume"+
+             " (a) "+str(round(solar.mean(),1))+"MW"+
+             " (b) "+str(round(wind.mean(),1))+"MW"+
+             " (c) "+str(round(agg.mean(),1))+"MW"+
+             " (d) "+str(round(demand.mean(),1))+"MW")
+
+titles = ["(a) Solar PV 100MWp Down","(b) Wind 100MWp Down","(c) Aggregator 100MWp Down","(d) Demand 100MWp Up (SL = 30MW)"]
+plt.legend(titles)
         
--- Design_VolumeGranularity.py
+++ Design_VolumeGranularity.py
@@ -21,16 +21,15 @@ DEMANDPEAK = 11742.29
 
 solar = solarRaw*100/SOLARINSTALLED
 wind = windRaw*100/WINDINSTALLED
-demand = demandRaw*100/DEMANDPEAK
+demand = demandRaw*100/DEMANDPEAK-30
 agg = solar*0.25 + wind*0.75
-x = np.linspace(0,5,200)
+x = np.arange(0,5,0.01)
 
 plt.close("all")
 fig, axes = plt.subplots(2,1)
 
-titles = ["(a) Solar PV 100MWp Down","(b) Wind 100MWp Down","(c) Aggregator 100MWp Down","(d) Demand 100MWp Up"]
-
-for i,data in enumerate([solar,wind,agg,demand-30]):
+#Calculate botht the impact on the minimum and the resolution
+for i,data in enumerate([solar,wind,agg,demand]):
 
     volume = pd.DataFrame(index = x, data={"gran":np.zeros(x.shape),"min":np.zeros(x.shape)})
 
@@ -38,30 +37,27 @@ for i,data in enumerate([solar,wind,agg,demand-30]):
         volume["gran"][j] = (data%size).mean()
         volume["min"][j] = data[data<size].sum()/data.shape[0]
         
-    axes[0].plot(volume["gran"]/data.mean()*100,linewidth=3)
-#    axes[0,i].set_xlabel('Volume Granularity [$\Delta$MW]')
-#    axes[0,i].set_ylabel('Lost Volume [%]')
-#    axes[0,i].set_xlim(0,5)
-#    axes[0,i].set_ylim(0,10)
-#    axes[0,i].set_title(titles[i]+"\nMean Effective Volume "+str(round(data.mean(),1)) +"MW")
-    
-    axes[1].plot(volume["min"].fillna(0)/data.mean()*100,linewidth=3)
- 
+    axes[0].plot(volume["gran"],linewidth=2)
+    axes[1].plot(volume["min"].fillna(0),linewidth=2)
+  
+titles = ["(a) Solar PV 100MWp Down","(b) Wind 100MWp Down","(c) Aggregator 100MWp Down","(d) Demand 100MWp Up (SL = 30MW)"]
+
+#Add labels to the axes 
 axes[0].legend(titles)
 axes[0].set_xlabel('Volume Granularity [$\Delta$MW]')
-axes[0].set_ylabel('Lost Volume [%]')
+axes[0].set_ylabel('Lost Volume [MW]')
 axes[0].set_xlim(0,5)
 axes[0].set_title('Lost Volume Versus Volume Granularity')
 
 axes[1].legend(titles)
 axes[1].set_xlabel('Volume Minimum [MW]')
-axes[1].set_ylabel('Lost Volume [%]')
+axes[1].set_ylabel('Lost Volume [MW]')
 axes[1].set_xlim(0,5)
 axes[1].set_title('Lost Volume Versus Volume Minimum')
 
-fig.suptitle("Mean Effective Volume"+
+fig.suptitle("Volume Constraints"+"\nMean Effective Volume"+
              " (a) "+str(round(solar.mean(),1))+"MW"+
              " (b) "+str(round(wind.mean(),1))+"MW"+
              " (c) "+str(round(agg.mean(),1))+"MW"+
-             " (d) "+str(round((demand-30).mean(),1))+"MW")
+             " (d) "+str(round((demand).mean(),1))+"MW")
 
